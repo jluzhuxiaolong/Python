496458f82f6b3c32a1a7e7b42cf207e294931fb4
jlu_xiaolong <2213346064@qq.com>
2016-08-17 09:44:25 +0800

    斐波那契数列，楼梯问题

---
+++ Person.py
@@ -0,0 +1,12 @@
+﻿class Person:
+	def __init__(self,name):
+		self.name=name
+	def SayHello(self):
+	    print ('My name is:',self.name);
+
+				  
+p = Person('p');
+p1 = Person('p1');
+print (p);
+p.SayHello();
+p1.SayHello();
---
+++ 楼梯问题.py
@@ -0,0 +1,22 @@
+﻿#created by xiaolong
+# 2016-08-17
+
+#上楼梯问题
+
+
+def UpStairs(n):
+	if(n<=0): 
+		return 1;
+	elif(n>1): 
+		return UpStairs(n-1)+UpStairs(n-2);
+	elif (n==1):
+		return 1;
+
+def Main(): 
+	k = int(input("输入楼梯层数:"));
+	if(k==0): 
+		print(0);
+	else:
+		print(UpStairs(k));
+		
+Main();
